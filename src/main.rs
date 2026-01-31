4e4f372f4a7a9759a25f75bea36402a3cace5895
agu <albin.gudmundsson@gmail.com>
2026-01-31 10:41:32 +0100

    use format macro for strings

--- src/main.rs
+++ src/main.rs
@@ -48,46 +48,37 @@ fn main() -> Result<()> {
     let mut watcher = notify::recommended_watcher(tx)?;
     watcher.watch(path, notify::RecursiveMode::Recursive)?;
 
-    // match path {
-    //     Some(value) => String::from(value),
-    //     None => String::from("awd"),
-    // };
-
     let mut has_changes = false;
+
+    println!("Starting to monitor changes at: {}", path.display());
 
     loop {
         match rx.recv_timeout(Duration::from_millis(*timeoutms)) {
             Ok(Ok(event)) => match event.kind {
                 EventKind::Create(_) | EventKind::Modify(_) | EventKind::Remove(_) => {
                     has_changes = true;
-                    println!("something changed");
                 }
                 _ => {}
             },
             Err(RecvTimeoutError::Timeout) => {
                 if has_changes {
-                    // TODO: prettier string interpolation and message from events
-                    let mut commit_command_string =
-                        String::from("cd -path- && git commit -m '-msg-'");
-                    commit_command_string =
-                        commit_command_string.replace("-path-", path.to_str().unwrap());
-
-                    println!("about to commit with command: {}", commit_command_string);
+                    // TODO: improve commit message
+                    let commit_message = String::from("message WIP");
 
                     if cfg!(target_os = "linux") {
                         let commit_command = Command::new("sh")
-                            .arg(commit_command_string)
+                            .arg(format!(
+                                "cd {} && git add -A && git commit -m '{}'",
+                                path.display(),
+                                commit_message
+                            ))
                             .output()
                             .expect("Failed to run commit command");
 
                         println!("commit status: {}", commit_command.status);
 
-                        let mut push_command_string = String::from("cd -path- && git push");
-                        push_command_string =
-                            push_command_string.replace("-path-", path.to_str().unwrap());
-
                         let push_command = Command::new("sh")
-                            .arg(push_command_string)
+                            .arg(format!("cd {} && git push", path.display()))
                             .output()
                             .expect("Failed to run commit command");
 
